6566e131d4a1fb571342688fb835b09c6498b8bb
dddd2018 <903344557@qq.com>
2020-05-04 18:10:18 +0800

    zhou

--- street/src/view/Onlinecourses.vue
+++ street/src/view/Onlinecourses.vue
@@ -1,11 +1,37 @@
 <template>
 	<div class="Onlinecourses">
-		在线课程
+		<div class="top">
+			<div class="btn_now">立即免费体验</div>
+		</div>
 	</div>
 </template>
 
 <script>
+
 </script>
 
-<style>
+<style scoped>
+	.top{
+		width:100%;
+		height: 500px;
+		background: url(https://wse.com.cn/_ui/responsive/theme-alpha/img/primecourse/prime-banner.jpg) top center no-repeat ;
+		position: relative;
+	}
+	.top .btn_now{
+		width:264px;
+		height: 53px;
+		border-radius: 5px;
+		font-size:18px;
+		font-weight: 600;
+		line-height: 53px;
+		text-align: center;
+		color: #fff;
+		background: #da4148;
+		position: absolute;
+		bottom: 100px;
+		right: 300px;
+	}
+	.btn_now:hover{
+		cursor:pointer
+	}
 </style>
