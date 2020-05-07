6e591eb1ffb0d71b567452c9e5451e7768f0e0e2
liqi <979447157@qq.com>
2020-05-07 19:37:42 +0800

    qiqi

--- huaerjie/src/view/contact.vue
+++ huaerjie/src/view/contact.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="contact">
-    <div class="c1181">contact</div>
+    <div class="c1181">
+    	<span>首页</span>
+    	<span class="colors">联系我们</span>
+    </div>
+   	<div class="lianx">
+   		<div class="xiaozi">联系我们</div>
+   	</div>
   </div>
 </template>
 
@@ -8,4 +14,17 @@
 </script>
 
 <style>
+	.c1181 span{
+		font-size: 16px;
+	}
+	.colors{
+		color: #ee2d7b;
+	}
+	.lianx{
+		margin-left: 120px;
+	}
+	.xiaozi{
+		font-size: 60px;
+		color: #ee2d42;
+	}
 </style>
